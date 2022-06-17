ad899754c1bab81de7820c43abff5199882f9c24
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-17 08:25:41 -0500

    UPDATE MULTI:  Clickable Button WORKS

--- js1.js
+++ js1.js
@@ -1,2 +1,9 @@
 // index.js
 
+const btn = document.querySelector('#btn')
+const output = document.querySelector('.container')
+// console.log(btn)
+
+btn.onclick = () => {
+  console.log('clicked')
+}
