158860edf431f97d6989bc8178836b8676e1cb48
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-17 08:28:43 -0500

    UPDATE MULTI:  Get DATA with Fetch AJAX request

--- js1.js
+++ js1.js
@@ -2,8 +2,14 @@
 
 const btn = document.querySelector('#btn')
 const output = document.querySelector('.container')
+const url = 'temp1.json'
 // console.log(btn)
 
 btn.onclick = () => {
   console.log('clicked')
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
 }
